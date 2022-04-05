558c711442eb644767afe8fe6116e7c86aefeb74
pablo-rmk <pablo.gutierrez.u@gmail.com>
2022-04-05 12:46:34 -0400

    Quinto commit: se agrega alerta a botón enviar de la sección Contacto; se adecua el ancho de la página reemplazando div-container por div-contenedor, con width y margen horizontal adecuado en el archivo CSS

---
+++ assets/js/myJs.js
@@ -0,0 +1,6 @@
+$(function () {
+    //Alerta
+    $('#boton-enviar').click(function () {
+        alert('Su solicitud ha sido recibida exitosamente. Muchas gracias por comunicarse con Viajes Chile.');
+    });
+});
