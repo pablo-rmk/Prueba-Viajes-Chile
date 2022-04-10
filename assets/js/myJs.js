1d2fb1c27b9c59061e3b154730c1e99a23e1d009
pablo-rmk <pablo.gutierrez.u@gmail.com>
2022-04-10 15:06:52 -0400

    Sexto commit: versión terminada para entrega

--- assets/js/myJs.js
+++ assets/js/myJs.js
@@ -1,6 +1,25 @@
 $(function () {
+
     //Alerta
     $('#boton-enviar').click(function () {
         alert('Su solicitud ha sido recibida exitosamente. Muchas gracias por comunicarse con Viajes Chile.');
+    });
+
+    // Smooth Scroll
+    $('.scroll').on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 500, function () {
+                window.location.hash = hash;
+            });
+        };
+    });
+    
+    // Activar Tooltips
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
     });
 });
